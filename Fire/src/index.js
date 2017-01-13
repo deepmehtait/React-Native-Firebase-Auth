3bba64800cca182b85c17e1f98b4360ef20fb813
Deep Mehta <deepmehta.it@live.com>
2017-01-12 23:02:37 -0800

    Adding Firebase and Login form setup.

--- Fire/src/index.js
+++ Fire/src/index.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, View } from 'react-native';
+import firebase from 'firebase';
+
 import { Header } from './components/common';
+import { CONFIG_KEYS } from './firebase-config';
+import LoginForm from './components/LoginForm';
 
 class Fire extends Component {
+  componentWillMount() {
+    firebase.initializeApp({ CONFIG_KEYS });
+  }
+
   render() {
     return (
       <View>
         <Header headerText='Fire Auth' />
-        <Text>
-          Fire.!
-        </Text>
+        <LoginForm />
       </View>
     );
   }
