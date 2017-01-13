5091be6c3a06407e5f55af1e320bac9fc2956f12
Deep Mehta <deepmehta.it@live.com>
2017-01-12 22:03:32 -0800

    Adding Common Styling components and eslint setup.

--- Fire/src/index.js
+++ Fire/src/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
+import { Header } from './components/common';
 
 class Fire extends Component {
   render() {
     return (
       <View>
+        <Header headerText='Fire Auth' />
         <Text>
-          Fire Auth.!
+          Fire.!
         </Text>
       </View>
     );
